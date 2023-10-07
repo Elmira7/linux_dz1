f4071417a5ec4c20184de2303fd92b3c9d1a45db
Elmira7 <90276363+Elmira7@users.noreply.github.com>
2023-10-07 23:56:38 +0300

    Update client.py

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 
 server_ip = input("input ip: ")
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((server_ip, 1706))
+client_socket.connect((server_ip, 1303))
 data = client_socket.recv(1024).decode()
 print({data})
 client_socket.close()
